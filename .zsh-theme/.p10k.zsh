2d40fe498d4ba6ee2f2e6a9bc24184a8c36ed72b
Alex Bosco <ajbosco00@gmail.com>
2025-02-13 15:38:35 -0500

    test

--- .zsh-theme/.p10k.zsh
+++ .zsh-theme/.p10k.zsh
@@ -125,9 +125,9 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
     # vpn_ip                # virtual private network indicator
-     load                  # CPU load
-     disk_usage            # disk usage
-     ram                   # free RAM
+    # load                  # CPU load
+    # disk_usage            # disk usage
+    # ram                   # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
@@ -141,7 +141,7 @@
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
      battery               # internal battery
-     wifi                  # wifi speed
+    # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
 
