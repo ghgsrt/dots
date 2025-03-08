eaad3c9a124c9985d287036046cd3bd7509155b0
ghgsrt <ajbosco00@gmail.com>
2025-03-08 21:00:01 +0000

    git subrepo clone git@github.com:ghgsrt/k-nvim.git home/.config/nvim
    subrepo:
      subdir:   "home/.config/nvim"
      merged:   "1858bd41"
    upstream:
      origin:   "git@github.com:ghgsrt/k-nvim.git"
      branch:   "master"
      commit:   "1858bd41"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "30db3b8"

---
+++ home/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,39 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'HiPhish/rainbow-delimiters.nvim',
+  'mbbill/undotree',
+  'aymericbeaumet/vim-symlink',
+  'theprimeagen/harpoon',
+  'tpope/vim-fugitive',
+  'geogitorg/neogit',
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      --"sindrets/diffview.nvim",        -- optional - Diff integration
+
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+  },
+  'nvim-treesitter/nvim-treesitter-context',
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+  {
+    'HiPhish/guile.vim',
+    lazy = false,
+    priority = 500,
+  },
+}
